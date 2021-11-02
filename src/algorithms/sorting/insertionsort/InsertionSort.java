1f70b2021fc9f9fa24707d39e5f40363c7013ac9
Valentina Servile <valentina.servile@gmail.com>
2021-11-02 19:15:02 +0100

    Insertion sort

---
+++ src/algorithms/sorting/insertionsort/InsertionSort.java
@@ -0,0 +1,26 @@
+package algorithms.sorting.insertionsort;
+
+public class InsertionSort {
+
+    public void sort(Integer[] input) {
+
+        for (int firstUnsortedIndex = 1; firstUnsortedIndex < input.length; firstUnsortedIndex++) {
+
+            Integer elementToInsert = input[firstUnsortedIndex];
+
+            int currentIndex;
+
+            for (currentIndex = firstUnsortedIndex; currentIndex > 0;  currentIndex--) {
+
+                if (elementToInsert <= input[currentIndex - 1]) {
+                    input[currentIndex] = input[currentIndex - 1];
+                } else {
+                    break;
+                }
+            }
+
+            input[currentIndex] = elementToInsert;
+        }
+    }
+
+}
