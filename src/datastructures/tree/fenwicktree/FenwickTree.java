ab2a8c7316de51dfd78d07c0c1e0dcb377e3b066
Valentina Servile <valentina.servile@gmail.com>
2022-01-14 17:42:12 +0100

    Implement Fenwick Tree

---
+++ src/datastructures/tree/fenwicktree/FenwickTree.java
@@ -0,0 +1,64 @@
+package datastructures.tree.fenwicktree;
+
+import static java.lang.System.arraycopy;
+
+public class FenwickTree {
+
+    private static final int PLACEHOLDER = 0;
+    private int[] backingArray;
+
+    //Complexity: O(n)
+    public FenwickTree(int[] numbers) {
+        backingArray = new int[numbers.length + 1];
+
+        backingArray[0] = PLACEHOLDER;
+        arraycopy(numbers, 0, backingArray, 1, numbers.length);
+
+        for (int i = 1; i < numbers.length; i++) {
+            int parentIndex = parentOf(i);
+            backingArray[parentIndex] += backingArray[i];
+        }
+    }
+
+    //Complexity: O(log(n))
+    public void addToIndex(int index, int value) {
+        int backingArrayIndex = index + 1;
+
+        while (backingArrayIndex < backingArray.length) {
+            backingArray[backingArrayIndex] += value;
+            backingArrayIndex = parentOf(backingArrayIndex);
+        }
+    }
+
+    //Complexity: O(log(n))
+    public int rangeQuery(int firstIndex, int secondIndex) {
+        return prefixSum(secondIndex) - prefixSum(firstIndex - 1);
+    }
+
+    //Complexity: O(log(n))
+    public int prefixSum(int index) {
+        int backingArrayIndex = index + 1;
+        int sum = backingArray[backingArrayIndex];
+
+        while (backingArrayIndex > 0) {
+            int nextIndex = childOf(backingArrayIndex);
+            sum += backingArray[nextIndex];
+            backingArrayIndex = nextIndex;
+        }
+
+        return sum;
+    }
+
+    private int parentOf(int index) {
+        return index + leastSignificantBitOf(index);
+    }
+
+    private int childOf(int index) {
+        return index - leastSignificantBitOf(index);
+    }
+
+    private int leastSignificantBitOf(int index) {
+        return index & -index; // Returns the value of the least significant bit which is set to 1
+    }
+
+}
---
+++ test/datastructures/tree/fenwicktree/FenwickTreeTest.java
@@ -0,0 +1,36 @@
+package datastructures.tree.fenwicktree;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FenwickTreeTest {
+
+    @Test
+    void prefixSumShouldReturnSumOfAllValuesUpToIndex() {
+        FenwickTree fenwickTree = new FenwickTree(new int[]{5, 7, 9, 12, -1, 7, 2, -4});
+
+        Integer result = fenwickTree.prefixSum(5);
+
+        assertEquals(39, result);
+    }
+
+    @Test
+    void rangeQueryShouldReturnDifferenceBetweenTwoIndexes() {
+        FenwickTree fenwickTree = new FenwickTree(new int[]{5, 7, 9, 12, -1, 7, 2, -4});
+
+        Integer result = fenwickTree.rangeQuery(3, 5);
+
+        assertEquals(18, result);
+    }
+
+    @Test
+    void shouldAddValueToIndexAdjustingOtherValuesInTheTree() {
+        FenwickTree fenwickTree = new FenwickTree(new int[]{5, 7, 9, 12, -1, 7, 2, -4});
+        fenwickTree.addToIndex(5, 2);
+
+        Integer result = fenwickTree.prefixSum(5);
+
+        assertEquals(41, result);
+    }
+}
