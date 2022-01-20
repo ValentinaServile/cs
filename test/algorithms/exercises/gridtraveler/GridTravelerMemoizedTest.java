591cebc42f44194d7ab40c92c9ca0ef13de17adf
Valentina Servile <valentina.servile@gmail.com>
2022-01-20 15:11:39 +0100

    Implement grid traveler traditional and memoized for comparison

---
+++ test/algorithms/exercises/gridtraveler/GridTravelerMemoizedTest.java
@@ -0,0 +1,23 @@
+package algorithms.exercises.gridtraveler;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GridTravelerMemoizedTest {
+
+    @Test
+    void shouldReturnNumberOfWaysAGridCanBeTraveled() {
+        Long result = GridTravelerMemoized.waysToTravelGridWithDimensions(2L, 3L);
+
+        assertEquals(3L, result);
+    }
+
+    @Test
+    void shouldReturnNumberOfWaysAGridCanBeTraveledQuicklyEvenForALargeGrid() {
+        Long result = GridTravelerMemoized.waysToTravelGridWithDimensions(18L, 18L);
+
+        assertEquals(2_333_606_220L, result);
+    }
+
+}
---
+++ test/algorithms/exercises/gridtraveler/GridTravelerTest.java
@@ -0,0 +1,24 @@
+package algorithms.exercises.gridtraveler;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GridTravelerTest {
+
+    @Test
+    void shouldReturnNumberOfWaysAGridCanBeTraveled() {
+        Long result = GridTraveler.waysToTravelGridWithDimensions(2L, 3L);
+
+        assertEquals(3L, result);
+    }
+
+    @Disabled("This computation takes too long")
+    @Test
+    void shouldReturnNumberOfWaysAGridCanBeTraveledSlowlyForALargeGrid() {
+        Long result = GridTraveler.waysToTravelGridWithDimensions(18L, 18L);
+
+        assertEquals(2_333_606_220L, result);
+    }
+}
